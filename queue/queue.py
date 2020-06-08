c5c78615ecbf2faa4a5621b15323f216df6f7b21
Liam Moore <liam.moore.dev@gmail.com>
2020-06-08 20:49:10 +0000

    implements queue with linked list

--- queue/queue.py
+++ queue/queue.py
@@ -13,45 +13,44 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
-class Queue:
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+    
+#     def __len__(self):
+#         return self.size
+
+#     def enqueue(self, value):
+#         self.size += 1
+#         self.storage.append(value)
+
+#     def dequeue(self):
+#         if self.size > 0:
+#             self.size -= 1
+#             return self.storage.pop(0)
+#         else: return None
+
+
+import sys
+import os
+sys.path.append(os.path.abspath('../singly_linked_list'))
+from singly_linked_list import LinkedList
+
+class Queue(LinkedList):
     def __init__(self):
+        super().__init__()
         self.size = 0
-        self.storage = []
     
     def __len__(self):
         return self.size
 
     def enqueue(self, value):
+        self.add_to_tail(value)
         self.size += 1
-        self.storage.append(value)
 
     def dequeue(self):
         if self.size > 0:
             self.size -= 1
-            return self.storage.pop(0)
+            return self.remove_head()
         else: return None
-
-
-# import sys
-# import os
-# sys.path.append(os.path.abspath('../singly_linked_list'))
-# from singly_linked_list import LinkedList
-
-# class Stack(LinkedList):
-#     def __init__(self):
-#         super().__init__()
-#         self.size = 0
-        
-
-#     def __len__(self):
-#         return self.size
-
-#     def push(self, value):
-#         self.add_to_tail(value)
-#         self.size += 1
-
-#     def pop(self):
-#         if self.size > 0:
-#             self.size -= 1
-#             return self.remove_tail()
-#         else: return None
