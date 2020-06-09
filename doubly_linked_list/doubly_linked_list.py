20e3611f12f001b8ab13ed0423f0e5ce04bdd550
Liam Moore <liam.moore.dev@gmail.com>
2020-06-09 20:11:31 +0000

    implements move to front and move to back

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -120,12 +120,24 @@ class DoublyLinkedList:
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
     def move_to_front(self, node):
-        pass
+        if node is not self.head:
+            if node is self.tail:
+                self.remove_from_tail()
+                self.add_to_head(node.value)
+            else:
+                self.delete(node)
+                self.add_to_head(node.value)
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
     def move_to_end(self, node):
-        pass
+        if node is not self.tail:
+            if node is self.head:
+                self.remove_from_head()
+                self.add_to_tail(node.value)
+            else:
+                self.delete(node)
+                self.add_to_tail(node.value)
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
