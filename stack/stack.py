f3e841fd976da6c7c394d8d5ee81858827a4c43c
Liam Moore <liam.moore.dev@gmail.com>
2020-07-15 21:04:03 +0000

    simplifies stack class

--- stack/stack.py
+++ stack/stack.py
@@ -10,16 +10,73 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+
+import sys
+sys.path.append('../singly_linked_list')
+from singly_linked_list import LinkedList
+
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return self.size
+
+#     def push(self, value):
+#         self.size += 1
+#         self.storage.append(value)
+
+#     def pop(self):
+#         if self.size == 0:
+#             return None
+#         else:
+#             self.size -= 1
+#             return self.storage.pop()
+
+
+class Node:
+    def __init__(self, value, next_node = None):
+        self.value = value
+        self.next = next_node
+
 class Stack:
     def __init__(self):
+        self.head = None
+        self.tail = None
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
 
     def __len__(self):
-        pass
+        return self.size
 
     def push(self, value):
-        pass
+        self.size += 1
+        if not self.head:
+            self.head = Node(value)
+            self.tail = self.head
+        elif self.head is self.tail:
+            self.head.next = Node(value)
+            self.tail = self.head.next
+        else:
+            self.tail.next = Node(value)
+            self.tail = self.tail.next
 
     def pop(self):
-        pass
+        if self.size == 0:
+            return None
+        else:
+            self.size -= 1
+            if self.head is self.tail:
+                value = self.head.value
+                self.head = None
+                self.tail = None
+            else:
+                value = self.tail.value
+                current = self.head
+                while(current.next is not self.tail):
+                    current = current.next
+                current.next = None
+                self.tail = current
+            return value
+            
